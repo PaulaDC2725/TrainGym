3bd4b95f9029426d73b005eddb18b8ee681d6517
PaulaDC2725 <pcdelgado3@misena.edu.co>
2021-10-20 16:38:56 -0500

    Correccion agendar instructor

--- views/AgendarIns.php
+++ views/AgendarIns.php
@@ -36,7 +36,7 @@ $filas1 = $consultas->cargarInstructorFiltroId($filtro);
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Registro Asistencias</title>
+    <title>Agendar a <?php echo $nombreInstructor?></title>
 	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon.png">
