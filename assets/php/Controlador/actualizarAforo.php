f8212d130026ea95e7e75c1c02ab2ca36bc09d97
PaulaDC2725 <pcdelgado3@misena.edu.co>
2021-10-20 10:47:29 -0500

    Arreglo error inicio sesion Recepcionista

--- assets/php/Controlador/actualizarAforo.php
+++ assets/php/Controlador/actualizarAforo.php
@@ -5,34 +5,7 @@ session_start();
 $numDoc = $_SESSION["NumeroIdentificacion"];
  $rolRec = $_SESSION["rolRecepcionista"];
  if ($rolRec != 1 )  {
-  echo '<!Doctype HTML>
-  <html lang="es-ES">
-  <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <meta name="description" content="" />
-  <meta name="author" content="" />
-<link
-href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
-rel="stylesheet"
-integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
-crossorigin="anonymous">
-<!--<link rel="stylesheet" href="../assets/css/style.css">-->
-<link rel="icon" type="image/x-icon" href="../../../assets/img/Logotipo.PNG" />
-  <!-- Core theme CSS (includes Bootstrap)-->
-<link href="../../../assets/css/style.css" rel="stylesheet" />
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.all.min.js"></script>
-  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-  <title>| Error</title>
-  </head>
-  <body>
-  <script> 
-  alert("Debe iniciar sesión correctamente para acceder!")
-  location.href = "../../../Views/index.php";
-  </script>
-  
-  </body>
-  </html>';
+  header('location: ../../../Views/Error.php');
   
 } else {
   
